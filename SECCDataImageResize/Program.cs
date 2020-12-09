da7d465c3f719981cc05c30a141bc4bb77736b42
songjoon <songjoon000@gmail.com>
2020-12-09 11:26:59 +0900

    초기 커밋.

---
+++ SECCDataImageResize/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SECCDataImageResize
+{
+    internal class Program
+    {
+        private static string rootFolder = @"F:\Data\";
+        private static Size targetSize = new Size(500, 500);
+
+        private static void Main(string[] args)
+        {
+            Stack<string> st = new Stack<string>();
+            st.Push(rootFolder);
+            while (st.Count != 0)
+            {
+                string r = st.Pop();
+                string[] dirs = Directory.GetDirectories(r);
+                foreach (var dir in dirs)
+                    st.Push(dir);
+                string[] files = Directory.GetFiles(r);
+                foreach (var file in files)
+                {
+                    FileInfo info = new FileInfo(file);
+                    if (info.Extension == ".png")
+                        continue;
+
+                    Bitmap origin = new Bitmap(file);
+
+                    Bitmap bitmap = new Bitmap(origin, targetSize);
+                    origin.Dispose();
+                    File.Delete(file);
+                    bitmap.Save(file);
+                }
+            }
+        }
+    }
+}
---
+++ SECCDataImageResizeWPF/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SECCDataImageResizeWPF
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private string path
+        {
+            get => pathTextBox.Text;
+            set => pathTextBox.Text = value;
+        }
+
+        public void pathDialog(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files (*.jpeg;*.jpg;*.png;*.PNG)|*.jpeg;*.jpg;*.png;*.PNG|JPEG files (*.jpeg)|*.jpeg|PNG files (*.PNG)|.PNG|All files (*.*)|*.*";
+            bool? res = dialog.ShowDialog();
+            if (res != true)
+                return;
+            path = dialog.FileName;
+        }
+
+        private readonly System.Drawing.Size targetSize = new System.Drawing.Size(500, 500);
+
+        public void transBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("파일을 지정하지 않았거나 경로가 존재하지 않습니다.");
+                return;
+            }
+            Bitmap bit = new Bitmap(path);
+
+            Bitmap resize = new Bitmap(bit, targetSize);
+            bit.Dispose(); File.Delete(path);
+            resize.Save(path);
+            resize.Dispose();
+            MessageBox.Show($"지정하신 파일의 크기가 {targetSize.ToString()}으로 변경되었습니다.");
+        }
+    }
+}
